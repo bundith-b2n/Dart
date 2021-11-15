0340e4414f12534e242a5b31e5b53a8a2d0ede50
Bundith <niphon.org@gmail.com>
2021-11-15 21:55:18 +0700

    Operator

---
+++ 6_ep_operator.dart
@@ -0,0 +1,18 @@
+// void main(List<String> args) {
+//   var x = 100, y = 50;
+//   var z = x + y;
+//   print(z);
+// }
+// +++++++++++++++++++++++++++++++++++++++++++++
+// void main(List<String> args) {
+//   int x = 100, y = 30;
+//   var z = x ~/ y;
+//   // ~cut
+//   print(z);
+// }
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+void main(List<String> args) {
+  int x = 100, y = 40;
+  int z = (x / y).toInt();
+  print(z);
+}
