2b7f6f6000fcf6958d116e2336de2a59a598fea5
Bundith <niphon.org@gmail.com>
2021-11-15 21:34:14 +0700

    5

--- 4_ep_constant_final.dart
+++ 4_ep_constant_final.dart
@@ -1,5 +1,9 @@
 void main(List<String> args) {
+  // const
   const int number = 100;
   // number = 500;
   print(number);
+  // final
+  final number2 = 150;
+  print(number2);
 }
---
+++ 5_ep_String.dart
@@ -0,0 +1,10 @@
+// void main(List<String> args) {
+//   var name = 'Nathan';
+//   String lname = "Jona";
+//   print(name + lname);
+// }
+void main(List<String> args) {
+  var name = "Nathan";
+  var age = 20;
+  print(name + '\t' + age.toString());
+}
