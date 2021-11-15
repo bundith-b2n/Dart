bf09212ef7dd3b9e277f5ad837381336a2ab354e
Bundith <niphon.org@gmail.com>
2021-11-15 21:02:29 +0700

    Data type_2

--- 2ep.dart
+++ 2ep.dart
@@ -1,2 +1,13 @@
 // variable
 // data type {bool,num,string,List<tpye>,Map<index,value>,Dynamic}
+
+void main(List<String> args) {
+  int age = 2;
+  double pi = 3.14;
+  String name = "Bundev Engineer";
+  bool isCheck = true;
+  print(isCheck);
+  print(age);
+  print(pi);
+  print(name);
+}
