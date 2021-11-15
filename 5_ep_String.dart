e7982d0c21e4bf438f56c30261491035883338a1
Bundith <niphon.org@gmail.com>
2021-11-15 21:44:44 +0700

    String

--- 5_ep_String.dart
+++ 5_ep_String.dart
@@ -3,8 +3,17 @@
 //   String lname = "Jona";
 //   print(name + lname);
 // }
+// void main(List<String> args) {
+//   var name = "Nathan";
+//   var age = 20;
+//   print(name + '\t' + age.toString());
+// }
 void main(List<String> args) {
   var name = "Nathan";
-  var age = 20;
-  print(name + '\t' + age.toString());
+  var city = "India";
+  var age = 43;
+  var number = 22;
+
+  // print("Name: $name \t Age: $age \t City: $city");
+  print("Name: $name \t Age: ${age - number} \t City: $city");
 }
