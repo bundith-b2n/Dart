eac40ea502334c6f2b94d84edb4565ecc9d513a8
Bundith <niphon.org@gmail.com>
2021-11-15 21:12:23 +0700

    2

---
+++ 3_ep_dynamic_type.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  var name = "Bundev";
+  var age = 43;
+  var salary = 40000;
+
+  // print(name);
+  // print(age);
+  // print(salary);
+  dynamic name_2 = "Engineer";
+  print(name);
+  print(name_2);
+}
