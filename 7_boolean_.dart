a22e10981ff57d30b405b074346c1e36a06405b5
Bundith <niphon.org@gmail.com>
2021-11-16 09:08:04 +0700

    while loop

--- 7_boolean_.dart
+++ 7_boolean_.dart
@@ -1,8 +1,25 @@
+// void main(List<String> args) {
+//   int x = 20, y = 30;
+//   print(x == y);
+//   print(x != y);
+//   print(x > y);
+//   print(x <= y);
+//   print(x < y);
+// }
+
+// void main(List<String> args) {
+//   int a = 10, b = 20;
+//   // print("a = $a");
+//   print("b = $b");
+//   // print("add Prefix: ${++a}");
+//   // print("add Postfix: ${a++}");
+//   print("add Prefix: ${--b}");
+//   // print("add Postfix: ${b--}");
+// }
+
 void main(List<String> args) {
-  int x = 20, y = 30;
-  print(x == y);
-  print(x != y);
-  print(x > y);
-  print(x <= y);
-  print(x < y);
+  int count = 1;
+  while (count <= 3) {
+    print("Hello $count");
+  }
 }
